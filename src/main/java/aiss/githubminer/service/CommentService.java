88cdff853ec8be57e8e333249775066442087559
Pablo2113 <pagonort@gmail.com>
2025-06-25 13:49:41 +0200

    Comment and Commit Service

--- src/main/java/aiss/githubminer/service/CommentService.java
+++ src/main/java/aiss/githubminer/service/CommentService.java
@@ -1,6 +1,41 @@
 package aiss.githubminer.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import aiss.githubminer.Utils;
+import aiss.githubminer.model.Comment;
+import aiss.githubminer.model.commentParser.CommentParser;
+import java.util.Arrays;
+import java.util.List;
+import java.util.SequencedCollection;
+
+@Service
 public class CommentService {
+
+    @Value("https://api.github.com")
+    private String defaultUri;
+
+    @Autowired
+    Utils utils;
+
+   
+    
+    public List<Comment> getCommentsUrl(String uri) {
+        ResponseEntity<CommentParser[]> response = utils.requestWithToken(uri, CommentParser[].class);
+        return Arrays.stream(response.getBody()).map(Comment::of).toList();
+    }
+
+    public SequencedCollection<Comment> getCommentsfromIssues(String owner, String repo, Integer issueIndex) {
+        String uri = defaultUri + "/repos/" + owner + "/" + repo +"/issues/" + issueIndex +"/comments";
+        return getCommentsUrl(uri);
+    }
+
+
+
+
+
 
 
 }
--- src/main/java/aiss/githubminer/service/CommitService.java
+++ src/main/java/aiss/githubminer/service/CommitService.java
@@ -1,4 +1,36 @@
 package aiss.githubminer.service;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.Arrays;
+import java.util.List;
+import aiss.githubminer.Utils;
+import aiss.githubminer.model.Commit;
+import aiss.githubminer.model.commitParser.CommitParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+@Service
 public class CommitService {
+
+    @Value("https://api.github.com")
+    private String apiUrl;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private Utils utils;
+
+
+    public List<Commit> getCommi(String uri) {
+        ResponseEntity<CommitParser[]> response = utils.requestWithToken(uri, CommitParser[].class);
+        List<Commit> commit = Arrays.stream(response.getBody()).map(Commit::of).toList();
+        return commit;
+    }
+
+
 }
